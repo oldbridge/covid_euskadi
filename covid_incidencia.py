1ed5cc11937512118b3a376e9b9dfb7bdaedd2b3
Xabi Zubi <xabi.zubi@gmail.com>
2021-01-11 18:36:39 +0100

    Move mostly to functions

--- covid_incidencia.py
+++ covid_incidencia.py
@@ -10,51 +10,21 @@ import requests
 import pandas as pd
 from matplotlib import pyplot as plt
 from datetime import datetime, timezone
+from functions import read_sample_data, get_populations, get_time_series, calculate_incidences
 
 if __name__ == '__main__':
     municipios_url = r"https://opendata.euskadi.eus/contenidos/ds_informes_estudios/covid_19_2020/opendata/generated/covid19-bymunicipality.json"
+    sample_data = "sample_data/covid19-bymunicipality_11012021.json"
+    with open(sample_data, 'r') as f:
+        datas = json.load(f)
     
-    resp = requests.get(url=municipios_url)
-    datas = resp.json() # Check the JSON Response Content documentation below
-    
-    cities = ["Irun", "Hondarribia"]
-    sizes = {}
+    cities = ["Irun", "Hondarribia", "Aizarnazabal"]
    
+    sizes = get_populations(datas)
     
-    # Get population of target cities
-    for data in datas['byMunicipalityByDate']['populationByMunicipalityByDate']:
-        city = data['dimension']['officialName']
-        if city in cities:
-            sizes[city] = data['values'][0]
+    df = get_time_series(datas, cities)
     
-    df = pd.DataFrame()
-    for data in datas['newPositivesByDateByMunicipality']:
-        date = data['date']
-        for i in data['items']:
-            count = i['positiveCount']
-            city = i['geoMunicipality']['officialName']
-            if city in cities:
-                df = df.append({'city': city, 
-                                'date': date, 
-                                'count': count}, ignore_index=True)
-    
-    df['date'] = pd.to_datetime(df['date'])
-    # Arreglar cagada de OpenData
-    for i, row in df.iterrows():
-        if (row['date'] <= datetime(2021, 12, 31, tzinfo=timezone.utc)) & (row['date'] > datetime(2021, 4, 1, tzinfo=timezone.utc)):
-            row['date'] = row['date'].replace(year=2020)
-            df.iloc[i] = row
-    
-    df.set_index("date", inplace=True)
-    # Calcula incidencias acumuladas
-    processed = pd.DataFrame()
-    for city in cities:
-        filt_df = df[df['city'] == city]
-        filt_df['incidencia7_abs'] = filt_df.rolling(7).sum()
-        filt_df['incidencia7_100k'] = filt_df['incidencia7_abs'] * 100000 / sizes[city]
-        processed = processed.append(filt_df)
-        
-
+    processed = calculate_incidences(df, cities, sizes)
     # Plot the incidencias acumuladas
     for city in cities:
         filt_df = processed[processed['city'] == city]
---
+++ functions.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 18:25:27 2021
+
+@author: Xabi
+"""
+
+
+import json
+import requests
+import pandas as pd
+from matplotlib import pyplot as plt
+from datetime import datetime, timezone
+
+def read_sample_data():
+    sample_data = "sample_data/covid19-bymunicipality_11012021.json"
+    with open(sample_data, 'r') as f:
+        datas = json.load(f)
+    return datas
+
+def get_populations(datas, cities):
+    sizes = {}
+    # Get population of target cities
+    for data in datas['byMunicipalityByDate']['populationByMunicipalityByDate']:
+        city = data['dimension']['officialName']
+        if city in cities:
+            sizes[city] = data['values'][0]
+    return sizes
+
+def get_time_series(datas, cities):
+    
+    df = pd.DataFrame()
+    for data in datas['newPositivesByDateByMunicipality']:
+        date = data['date']
+        for i in data['items']:
+            count = i['positiveCount']
+            city = i['geoMunicipality']['officialName']
+            if city in cities:
+                df = df.append({'city': city, 
+                                'date': date, 
+                                'count': count}, ignore_index=True)
+    
+    df['date'] = pd.to_datetime(df['date'])
+    
+    # Arreglar cagada de OpenData
+    for i, row in df.iterrows():
+        if (row['date'] <= datetime(2021, 12, 31, tzinfo=timezone.utc)) & (row['date'] > datetime(2021, 4, 1, tzinfo=timezone.utc)):
+            row['date'] = row['date'].replace(year=2020)
+            df.iloc[i] = row
+    
+    df.set_index("date", inplace=True)
+    return df
+
+def calculate_incidences(df, cities, sizes):
+    # Calcula incidencias acumuladas
+    processed = pd.DataFrame()
+    for city in cities:
+        filt_df = df[df['city'] == city]
+        filt_df['incidencia7_abs'] = filt_df.rolling(7).sum()
+        filt_df['incidencia7_100k'] = filt_df['incidencia7_abs'] * 100000 / sizes[city]
+        processed = processed.append(filt_df)
+    
+    return processed
